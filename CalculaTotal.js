cedb78aeb3385e048c220f0eebe14e68a3f46769
TiooDark <145171630+TiooDark@users.noreply.github.com>
2024-03-01 00:48:34 -0300

    Update CalculaTotal.js

--- CalculaTotal.js
+++ CalculaTotal.js
@@ -9,6 +9,9 @@ clientes.forEach(function(cliente) {
     // Captura o valor unitário do produto
     var unitario = parseFloat(cliente.querySelector(".info-valor").textContent);
 
+    // Captura o valor total do produto
+    var total = parseFloat(cliente.querySelector(".info-total").textContent);
+
     // Valida a quantidade
     if (qtde < 1 || isNaN(qtde)) {
         // Quantidade NOK, avise o usuário
@@ -18,6 +21,27 @@ clientes.forEach(function(cliente) {
         // Quantidade OK, pode prosseguir
         // Calcula o valor total da encomenda
         cliente.querySelector(".info-total").textContent = calculaTotal(qtde, unitario);
+    }
+
+    // Valida a quantidade
+    if (qtde < 1 || isNaN(unitario)) {
+        // Quantidade NOK, avise o usuário
+        cliente.querySelector(".info-valor").textContent = "INVÁLIDO!";
+        cliente.style.backgroundColor = "red";
+    } else {
+        // Quantidade OK, pode prosseguir
+        // Calcula o valor total da encomenda
+        cliente.querySelector(".info-total").textContent = calculaTotal(qtde, unitario);
+    }
+
+    if(total < 1 || isNaN(qtde)){
+        cliente.querySelector(".info-total").textContent = "-";
+        cliente.querySelector(".info-valor").textContent = "-";
+    }
+
+    if(total < 1 || isNaN(unitario)){
+        cliente.querySelector(".info-total").textContent = "-";
+        cliente.querySelector(".info-qtde").textContent = "-";
     }
 });
 
